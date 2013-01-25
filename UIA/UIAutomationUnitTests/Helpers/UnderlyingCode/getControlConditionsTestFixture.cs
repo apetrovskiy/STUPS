6009adfd132596ed6891423722aa1f7edd559c84
apetrovskiy <uiautomation@hotmail.com>
2013-01-25 21:34:08 +0400

    Added unit test support to the UIAutomation project.

---
+++ UIA/UIAutomationUnitTests/Helpers/UnderlyingCode/getControlConditionsTestFixture.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 1/25/2013
+ * Time: 8:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace UIAutomationUnitTests
+{
+    using System;
+    using PSTestLib;
+    using UIAutomation;
+    using MbUnit.Framework;
+    
+    /// <summary>
+    /// Description of getControlConditionsTestFixture.
+    /// </summary>
+    [TestFixture]
+    public class getControlConditionsTestFixture
+    {
+        public getControlConditionsTestFixture()
+        {
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            UnitTestingHelper.PrepareUnitTestDataStore();
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+        }
+        
+        [Test]
+        [Category("Fast")]
+        [Ignore]
+        public void Need_Code()
+        {
+            
+        }
+    }
+}
---
+++ UIA/UIAutomationUnitTests/Helpers/UnitTestingHelper.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 1/25/2013
+ * Time: 9:28 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace UIAutomationUnitTests
+{
+    using System;
+    using UIAutomation;
+    using UIAutomation.Commands;
+    using PSTestLib;
+    
+    /// <summary>
+    /// Description of UnitTestingHelper.
+    /// </summary>
+    public static class UnitTestingHelper
+    {
+        static UnitTestingHelper()
+        {
+        }
+        
+        public static void PrepareUnitTestDataStore()
+        {
+            PSCmdletBase.UnitTestMode = true;
+            
+            if (0 < PSTestLib.UnitTestOutput.Count) {
+
+                PSTestLib.UnitTestOutput.Clear();
+            }
+            
+            UIAutomation.CurrentData.ResetData();
+            
+        }
+    }
+}

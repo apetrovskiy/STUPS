402af37acbd21ec65ebd24a629fdae34f7611035
apetrovskiy <uiautomation@hotmail.com>
2013-01-27 12:27:01 +0400

    Unit test for checking parameters (the Get-UIAControl cmdlet).

--- TMX/TLAddinUnitTests/ParamChecks/Settings.cs
+++ TMX/TLAddinUnitTests/ParamChecks/Settings.cs
@@ -7,7 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 
-namespace TLAddinUnitTests
+namespace TLAddinUnitTests.CheckCmdletParameters
 {
     using System;
     
---
+++ UIA/UIAutomationUnitTests/ParamChecks/Get/GetUIAControlCommandTestFixture.cs
@@ -0,0 +1,104 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: shuran
+ * Date: 1/27/2013
+ * Time: 12:02 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace UIAutomationUnitTests.CheckCmdletParameters
+{
+    using System;
+    using System.Windows.Automation;
+    using PSTestLib;
+    using UIAutomation;
+    using MbUnit.Framework;
+    
+    /// <summary>
+    /// Description of GetUIAControlCommandTestFixture.
+    /// </summary>
+    [TestFixture]
+    public class GetUIAControlCommandTestFixture
+    {
+        public GetUIAControlCommandTestFixture()
+        {
+        }
+        
+        [SetUp]
+        public void PrepareRunspace()
+        {
+            MiddleLevelCode.PrepareRunspaceForParamChecks();
+        }
+        
+        [TearDown]
+        public void DisposeRunspace()
+        {
+            MiddleLevelCode.DisposeRunspace();
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_NoParameters()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl;");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_ControlType()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -ControlType Button;");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_Name()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -Name 'aaa';");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_ControlType_Name()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -ControlType Button -Name 'aaa';");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_AutomationId()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -AutomationId '111';");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_ControlType_AutomationId()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -ControlType Button -AutomationId '111';");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_ClassName()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -Class 'class';");
+        }
+        
+        [Test]
+        [Category("Fast")]
+        public void Get_UIAControl_ControlType_ClassName()
+        {
+            CmdletUnitTest.TestRunspace.RunAndCheckCmdletParameters(
+        		"Get-UIAControl -ControlType Button -Class 'class';");
+        }
+    }
+}

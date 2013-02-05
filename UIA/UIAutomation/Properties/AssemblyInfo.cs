65af35a24f5daa4961d76bd83c69441155308b69
apetrovskiy <uiautomation@hotmail.com>
2013-02-05 22:29:43 +0400

    started UIAutomation 0.8.5

--- UIA/UIAutomation/Properties/AssemblyInfo.cs
+++ UIA/UIAutomation/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.4.*")]
+[assembly: AssemblyVersion("0.8.5.*")]

3a9979c66b1566f9500ea76626c8628154b08b50
apetrovskiy <uiautomation@hotmail.com>
2013-04-16 19:15:18 +0400

    .

---
+++ Win32Utils/Helpers/Inheritance/PrivilegeCmdletBase.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 4/15/2013
+ * Time: 1:46 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace Win32Utils
+{
+    using System;
+    using System.Management.Automation;
+    
+    /// <summary>
+    /// Description of PrivilegeCmdletBase.
+    /// </summary>
+    public class PrivilegeCmdletBase : SecurityCmdletBase
+    {
+        public PrivilegeCmdletBase()
+        {
+        }
+    }
+}

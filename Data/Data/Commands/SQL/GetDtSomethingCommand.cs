c8043080ce27a08daab97e602735ba6c6e93b8e1
apetrovskiy <uiautomation@hotmail.com>
2013-05-27 11:59:04 +0400

    .

---
+++ Data/Data/Commands/SQL/GetDtSomethingCommand.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 5/27/2013
+ * Time: 11:18 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace Data.Commands
+{
+    using System;
+    using System.Management.Automation;
+    using System.Reflection;
+    
+    /// <summary>
+    /// Description of GetDtSomethingCommand.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "DtSomething")]
+    public class GetDtSomethingCommand : PSCmdlet
+    {
+        public GetDtSomethingCommand()
+        {
+        }
+        
+        [Parameter(Mandatory = true,
+                   ValueFromPipeline = true,
+                   Position = 0)]
+        public object InputObject { get; set; }
+        
+        protected override void ProcessRecord()
+        {
+            
+            if (null != this.InputObject) {
+                
+                this.WriteObject(this.InputObject.GetType().Name);
+                
+                PropertyInfo[] properties =
+                    this.InputObject.GetType().GetProperties();
+                
+                if (null != properties) {
+                    
+                    foreach (PropertyInfo prop in properties) {
+                        
+                        this.WriteObject(prop.Name + "\t" + prop.PropertyType.Name);
+                    }
+                }
+                
+                this.WriteObject("Properties:" + "\t" + ((PSObject)this.InputObject).Properties.GetType().Name);
+                foreach (var prop in ((PSObject)this.InputObject).Properties) {
+                    
+                    this.WriteObject(prop.Name + "\t" + prop.Value);
+                    
+                }
+                
+                
+            }
+            
+        }
+    }
+}

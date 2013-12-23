c8edaaf186a2c411b3ec3767e3e4a2cce4452a81
apetrovskiy <uiautomation@hotmail.com>
2013-12-23 22:14:53 +0400

    extension of the elements' object model

---
+++ UIA/UIAutomation/Interface/ISupportsNavigation.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 11/5/2013
+ * Time: 1:31 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace UIAutomation
+{
+	public interface ISupportsNavigation
+	{
+		IUiElement NavigateToParent();
+		IUiElement NavigateToFirstChild();
+		IUiElement NavigateToLastChild();
+		IUiElement NavigateToNextSibling();
+		IUiElement NavigateToPreviousSibling();
+	}
+}

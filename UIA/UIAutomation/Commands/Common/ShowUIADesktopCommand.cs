65389e8e69cee6b0246818e0c9527fe82abeadb9
apetrovskiy <uiautomation@hotmail.com>
2014-02-10 23:15:22 +0400

    experimental refactoring of commands (that are under cmdlets)

--- UIA/UIAutomation/Commands/Common/ShowUIADesktopCommand.cs
+++ UIA/UIAutomation/Commands/Common/ShowUIADesktopCommand.cs
@@ -23,42 +23,15 @@ namespace UIAutomation.Commands.Common
         protected override void BeginProcessing()
         {
             try{
-                // 20131109
-                //AutomationElement showDesktopButton =
-                // 20131209
-                /*
-                IUiElement showDesktopButton =
-                    // 20131109
-                    //AutomationElement.RootElement.FindFirst(
-                    UiElement.RootElement.FindFirst(
-                        TreeScope.Children,
-                        new PropertyCondition(
-                            AutomationElement.NameProperty,
-                            "Show desktop"));
-                */
-                IUiElement showDesktopButton =
+                var showDesktopButton =
                     UiElement.RootElement.FindFirst(
                         TreeScope.Children,
                         new PropertyCondition(
                             AutomationElement.ClassNameProperty,
                             "TrayShowDesktopButtonWClass"));
                 
-                // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                // 20131208
-                // InvokePattern invPtrn =
-                    // 20131208
-                    // showDesktopButton.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
-                //    showDesktopButton.GetCurrentPattern<IInvokePattern, InvokePattern>(InvokePattern.Pattern) as InvokePattern;
-                // invPtrn.Invoke();
-                // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                IInvokePattern invPtrn =
-                    showDesktopButton.GetCurrentPattern<IInvokePattern>(InvokePattern.Pattern);
-                invPtrn.Invoke();
-                /*
-                InvokePattern invPtrn = 
-                    showDesktopButton.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
-                invPtrn.Invoke();
-                */
+                showDesktopButton.PerformInvoke();
+                
                 WriteObject(this, true);
             }
             catch (Exception ee) {

2683004b439d02b1d37235163fd4c23f7047d7d0
apetrovskiy <uiautomation@hotmail.com>
2014-07-22 15:12:35 +0400

    .

---
+++ TMX/Tmx.Server/Modules/TestTasksModule.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 7/21/2014
+ * Time: 10:16 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Nancy;
+
+namespace Tmx.Server.Modules
+{
+	/// <summary>
+	/// Description of TestTasksModule.
+	/// </summary>
+	public class TestTasksModule : NancyModule
+	{
+		public TestTasksModule() : base("/tasks")
+		{
+			
+		}
+	}
+}

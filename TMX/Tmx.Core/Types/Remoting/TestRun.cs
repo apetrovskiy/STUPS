2c32a19886668a83bc5dbf55a51df583652abe7b
apetrovskiy <apetrovskiy@hotmail.com>
2014-11-22 12:30:56 +0300

    modified:   TMX/Tmx.Core/Types/Remoting/TestRun.cs

--- TMX/Tmx.Core/Types/Remoting/TestRun.cs
+++ TMX/Tmx.Core/Types/Remoting/TestRun.cs
@@ -24,7 +24,6 @@ namespace Tmx.Core.Types.Remoting
 		public TestRun()
 		{
             Data = new CommonData();
-            // 20141106
             TestSuites = new List<ITestSuite>();
             TestPlatforms = new List<ITestPlatform> {
                 new TestPlatform {
@@ -42,7 +41,6 @@ namespace Tmx.Core.Types.Remoting
         public string Name { get; set; }
         public string Description { get; set; }
         public ICommonData Data { get; set; }
-        // 20141106
         public List<ITestSuite> TestSuites { get; set; }
         public List<ITestPlatform> TestPlatforms { get; set; }
         // public ListOfTestSuites TestSuites { get; set; }

39c0061a3bdab646803f8c8ebdfd26ab33f95e55
apetrovskiy <uiautomation@hotmail.com>
2015-02-06 01:14:33 +0300

    .

--- TMX/TMX/Properties/AssemblyInfo.cs
+++ TMX/TMX/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TMX")]
+[assembly: AssemblyTitle("Tmx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexander Petrovskiy")]

e97df6b6aea6a3c24325c16255d34fa162d0085d
apetrovskiy <apetrovskiy@hotmail.com>
2015-04-07 21:51:26 +0300

    refactoring

--- TMX/Tmx.Client.Runner/Program.cs
+++ TMX/Tmx.Client.Runner/Program.cs
@@ -6,10 +6,13 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-using System;
 
 namespace Tmx.Client.Runner
 {
+    using System;
+    using System.ServiceProcess;
+    using System.Threading;
+
     class Program
     {
         public static void Main(string[] args)
@@ -20,6 +23,33 @@ namespace Tmx.Client.Runner
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
+            /*
+            try
+            {
+                ServerControl.Port = 12340;
+                ServerControl.Start(@"http://localhost:" + 12340);
+
+                if (!Environment.UserInteractive)
+                {
+                    var services = new ServiceBase[] { new ServiceControl() };
+                    ServiceBase.Run(services);
+                }
+                else
+                {
+                    var service = new ServiceControl();
+                    Thread.Sleep(Timeout.Infinite);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.InnerException.Message);
+            }
+            Console.Write(@"Press any key to stop server . . . ");
+            Console.ReadKey(true);
+            ServerControl.Stop();
+            */
         }
     }
 }
---
+++ TMX/Tmx.Server/ObjectModel/ServerControl/ServiceControl.cs
@@ -0,0 +1,21 @@
+﻿namespace Tmx.Server
+{
+    using System.ServiceProcess;
+
+    public class ServiceControl : ServiceBase
+    {
+        //public ServiceControl()
+        //{
+        //}
+        //
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        } 
+    }
+}
